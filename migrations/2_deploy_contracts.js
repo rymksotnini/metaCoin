0fd293b519356e8e1980259bdd18202b19000e11
rached1997 <hawejdene@gmail.com>
2020-05-30 17:42:49 +0100

    remove meta coin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,5 @@ var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Clubisti =  artifacts.require("./Clubisti.sol");
 module.exports = function(deployer) {
-  //deployer.deploy(ConvertLib);
-  //deployer.link(ConvertLib, MetaCoin);
- // deployer.deploy(MetaCoin);
   deployer.deploy(Clubisti);
 };
