d7a3ffb69d59bc5fa169ae38d2130d5ccfd72424
rymksontini <rymksontini@gmail.com>
2020-05-31 22:03:48 +0100

    template and shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {SharedModule} from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
     BrowserAnimationsModule,
     CommonModule,
     MatButtonModule,
